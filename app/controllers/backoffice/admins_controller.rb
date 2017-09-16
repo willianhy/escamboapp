a9446236a7de896e7bb335cb1d2b7a6381ade193
Willian Yassunaka <willianhy@gmail.com>
2017-09-16 10:54:51 -0300

    active record scope

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -2,8 +2,7 @@ class Backoffice::AdminsController < BackofficeController
   before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
-    #@admins = Admin.all
-    @admins = Admin.with_full_access
+    @admins = Admin.full_access
   end
 
   def new
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,12 @@
 class Admin < ActiveRecord::Base
+  # Constants
+  ROLES  = {:full_access => 0, :restricted_access => 1}
+
+  #Enums
   enum role: {:full_access => 0, :restricted_access => 1}
 
-  scope :with_full_access, -> { where(role: 'full_access')}
+  scope :with_full_access, -> { where(role: ROLES[:full_access])}
+  scope :with_restricted_access, -> { where(role: ROLES[:restricted_access])}
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable

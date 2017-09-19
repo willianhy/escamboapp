13adc4fffbec72523f942fc26e19b73ae43cfabd
Willian Yassunaka <willianhy@gmail.com>
2017-09-19 00:11:47 -0300

    sending e-mail from modal

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -9,5 +9,10 @@ class Backoffice::SendMailController < ApplicationController
   end
 
   def create
+    AdminMailer.send_message(current_admin, params[:'recipient-text'], params[:'subject-text'], params[:'message-text']).deliver_now
+
+    respond_to do |format|
+      format.js
+    end
   end
 end

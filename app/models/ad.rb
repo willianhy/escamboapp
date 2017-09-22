88c6b0f3c3b3d374ad5fe3c8a4d593b24ab94876
Willian Yassunaka <willianhy@gmail.com>
2017-09-21 22:01:06 -0300

    refactoring...

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,6 +2,8 @@ class Ad < ActiveRecord::Base
   belongs_to :member
   belongs_to :category
 
+  scope :last_six, -> { limit(6).order(created_at: :desc) }
+
   # paperclip
   has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/

1aa61983cb0b9f9268391fdb7015866c24104680
Willian Yassunaka <willianhy@gmail.com>
2017-09-23 14:35:15 -0300

    passing parameters to scope

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,7 +2,8 @@ class Ad < ActiveRecord::Base
   belongs_to :member
   belongs_to :category
 
-  scope :last_six, -> { limit(6).order(created_at: :desc) }
+  scope :descending_order, -> (quantity = 10) { limit(quantity).order(created_at: :desc) }
+  scope :to_the, -> (member) { where(member: member) }
 
   # paperclip
   has_attached_file :picture, styles: { medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"

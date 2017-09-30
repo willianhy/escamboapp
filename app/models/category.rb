e0f247bf9ce75d313e185ba9e24f03a78ee6006c
Willian Yassunaka <willianhy@gmail.com>
2017-09-29 22:15:27 -0300

    implementing counter_cache to category/ads

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ActiveRecord::Base
+
+  # Associations
+  has_many :ads
+
+  # Validations
   validates_presence_of :description
 
   scope :order_by_description, -> { order(:description) }

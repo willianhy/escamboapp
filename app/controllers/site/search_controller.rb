51a71a640627a01c30739cf711fb0dfd90a2b160
Willian Yassunaka <willianhy@gmail.com>
2017-11-15 15:41:57 -0200

    elasticsearch

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -2,7 +2,7 @@ class Site::SearchController < SiteController
 
   def ads
   	@categories = Category.order_by_description
-    @ads = Ad.search(params[:q], params[:page])
+    @ads = Ad.search(params[:q], fields: [:title], page: params[:page], per_page: Ad::QTT_PER_PAGE)
 
     cookies[:search_term] = params[:q]
     cookies[:categories] = @categories.to_json
